9a4176de1dbadcd659fa0f5a69998a95b13fc477
alexpopa2402 <alexpopa2402@yahoo.com>
2025-06-16 13:21:33 +0200

    commented out error.message div to give less info to user which is of no interest

--- src/components/error-boundary/ErrorFallback.jsx
+++ src/components/error-boundary/ErrorFallback.jsx
@@ -5,8 +5,8 @@ const ErrorFallback = ({ error, resetErrorBoundary }) => {
   console.log('ErrorFallback rendered with error:', error.message);
     return (
       <div role="alert" className='error-fallback-container'>
-        <p>Something went wrong:</p>
-        <div>{error.message}</div>
+        <p>Something went wrong</p>
+{/*         <div>{error.message}</div> */}
         <button onClick={resetErrorBoundary}>Try again</button>
       </div>
     );
